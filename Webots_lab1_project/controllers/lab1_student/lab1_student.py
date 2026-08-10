f6c24ac0d1e28547f3949a2fcfe22b2ac3342109
Benaja Kuhn <benaja.kuhn@stud.unibas.ch>
2026-08-10 14:16:41 +1000

    task 6 done

--- Webots_lab1_project/controllers/lab1_student/lab1_student.py
+++ Webots_lab1_project/controllers/lab1_student/lab1_student.py
@@ -19,7 +19,7 @@ right_motor.setVelocity(0.0)
 proximity_sensors = []
 for index in range(8):
     sensor = robot.getDevice(f"ps{index}")
-    # TODO 3: Enable each sensor using TIME_STEP.
+    sensor.enable(TIME_STEP)
     proximity_sensors.append(sensor)
 
 MAX_SPEED = 6.28
@@ -29,23 +29,28 @@ OBSTACLE_THRESHOLD = 80.0
 print("My first Webots controller is running")
 while robot.step(TIME_STEP) != -1:
     # print(f"simulation time = {robot.getTime():.2f} s")
-    # TODO 4: Read all eight sensor values.
+
     values = [0.0] * 8
+    values = [sensor.getValue() for sensor in proximity_sensors] 
+    print([round(value, 1) for value in values])
 
-    # TODO 5: Compute obstacle strengths on the right and left sides.
-    right_obstacle = 0.0
-    left_obstacle = 0.0
+    right_obstacle = max(values[0], values[1], values[2])
+    left_obstacle = max(values[5], values[6], values[7])
+    
+    left_speed = CRUISE_SPEED
+    right_speed = CRUISE_SPEED
+    
+    if left_obstacle > OBSTACLE_THRESHOLD or right_obstacle > OBSTACLE_THRESHOLD:
+        if left_obstacle > right_obstacle:
+            # Obstacle is stronger on the left: turn right. 
+            left_speed = 0.45 * MAX_SPEED
+            right_speed = -0.25 * MAX_SPEED
+        else:
+            # Obstacle is stronger on the right: turn left. 
+            left_speed = -0.25 * MAX_SPEED
+            right_speed = 0.45 * MAX_SPEED
 
-    #left_speed = CRUISE_SPEED
-    #right_speed = CRUISE_SPEED
-    
-    left_speed = -2.0
-    right_speed = 2.0
-    
     left_motor.setVelocity(left_speed)
     right_motor.setVelocity(right_speed)
-
-    # TODO 6: If an obstacle is detected, turn away from the stronger signal.
-
-    # TODO 7: Send the calculated speeds to the wheel motors.
+    
     pass
